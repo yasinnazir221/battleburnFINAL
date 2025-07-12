4bc8984d7ab61dc2beb2b686c0713f8eb200d744
yasinnazir221 <gamingcomminty@gmail.com>
2025-07-12 14:12:23 +0100

    Updated config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,12 +6,7 @@ import { getStorage } from 'firebase/storage';
 // Firebase configuration
 // Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDw3JkPokn-Vr87Z885CAKjvrmajfFNLts",
-  authDomain: "battleburn-a6fe0.firebaseapp.com",
-  projectId: "battleburn-a6fe0",
-  storageBucket: "battleburn-a6fe0.firebasestorage.app",
-  messagingSenderId: "647937146440",
-  appId: "1:647937146440:web:4c9ddfdf12d4eddedae27d"
+
 };
 
 // Initialize Firebase
